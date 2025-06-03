9ea3a20b1d0c4a1db78319dc8416285f1d2789ee
Fernández Pereira <miguel17693@hotmail.com>
2025-06-03 13:52:40 +0200

    optimizing pupeteer

--- src/modules/bot/bot.module.ts
+++ src/modules/bot/bot.module.ts
@@ -12,13 +12,23 @@ import { EventEmitter2 } from '@nestjs/event-emitter';
   providers: [
     {
       provide: Client,
-      useFactory: () => new Client({
-        authStrategy: new LocalAuth(),
-        puppeteer: {
-          headless: true,
-          args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        },
-      }),
+      useFactory: () =>
+        new Client({
+          authStrategy: new LocalAuth(),
+          puppeteer: {
+            headless: true,
+            args: [
+              '--no-sandbox',
+              '--disable-setuid-sandbox',
+              '--disable-dev-shm-usage',
+              '--disable-accelerated-2d-canvas',
+              '--no-first-run',
+              '--no-zygote',
+              '--single-process',
+              '--disable-gpu',
+            ],
+          },
+        }),
     },
     BotService,
     ScheduleService,
